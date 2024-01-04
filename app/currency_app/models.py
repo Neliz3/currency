54daf976c0056e1cff86d78793d7e5845f3d8550
Neliz3 <elizaa.nadrega14@gmail.com>
2024-01-04 15:26:37 +0200

    models & ci are added

--- app/currency_app/models.py
+++ app/currency_app/models.py
@@ -1,3 +1,15 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Rate(models.Model):
+    currency = models.CharField(max_length=5)
+    sale = models.DecimalField(max_digits=5, decimal_places=2)
+    buy = models.DecimalField(max_digits=5, decimal_places=2)
+    created = models.DateTimeField(auto_now_add=True)
+    source = models.CharField(max_length=70)
+
+
+class ContactUs(models.Model):
+    email_from = models.EmailField()
+    subject = models.CharField(max_length=120)
+    message = models.TextField()
