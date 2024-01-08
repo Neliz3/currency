249a20ec5950dfb0fc8fb5bed1da3ed408c62589
Neliz3 <elizaa.nadrega14@gmail.com>
2024-01-08 07:52:01 +0200

    model Options added for experiments

--- app/currency_app/models.py
+++ app/currency_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import F
 
 
 class Rate(models.Model):
@@ -13,3 +14,16 @@ class ContactUs(models.Model):
     email_from = models.EmailField()
     subject = models.CharField(max_length=120)
     message = models.TextField()
+
+
+class Options(models.Model):
+    b = models.BooleanField(default=True)
+    datetime_add = models.DateTimeField(auto_now_add=True)
+    datetime_saved = models.DateTimeField(auto_now=True)
+    duration = models.DurationField()
+    duration.db_index = True
+
+    duration2 = models.GeneratedField(
+        expression=F("datetime_saved") - F("datetime_add"),
+        output_field=models.DurationField(),
+        db_persist=True)
